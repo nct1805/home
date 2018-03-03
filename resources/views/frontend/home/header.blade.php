a01d995ad23283d5d7d670d7d0f02f75072f621b
Hoàng Nam <hoangnam750@gmail.com>
2018-03-03 15:40:35 +0700

    thay doi text,link

--- resources/views/frontend/home/header.blade.php
+++ resources/views/frontend/home/header.blade.php
@@ -2,16 +2,16 @@
 <div class="container">
 <div class="row">
 	<div class="col-lg-3 col-md-3 col-xs-12 hidden-sm hidden-xs header-logo">
-    <a href="/5giay" title="#"><img class="img-thumbnail" src="public/frontend/images/images/logo.png" title="#" alt="#"/></a>    
+    <a href="/5giay" title="5 giây cho mỗi lần thấy"><img class="img-thumbnail" src="public/frontend/images/images/logo.png" title="5 giây cho mỗi lần thấy" alt="5 giây cho mỗi lần thấy"/></a>    
     </div>    
     <div class="col-lg-3 col-md-3 register pull-right hidden-sm hidden-xs">    
     	<div class="btn-group">
-          <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" onclick="location.href='<?=$url_5giay;?>';">
+          <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
             <span class="text-btn">Đăng nhập & đăng ký</span> <span class="caret"></span>
           </button>
           <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Đăng ký</a></li>
-            <li><a href="#">Đăng nhập</a></li>
+            <li><a href="https://www.5giay.vn/login/login" title="Đăng ký">Đăng ký</a></li>
+            <li><a href="https://www.5giay.vn/login/login" title="Đăng nhập">Đăng nhập</a></li>
           </ul>
         </div> 
     </div>
@@ -34,51 +34,18 @@
 <span class="icon-bar"></span>
 </a>
 <span class="sologan_mobile">
-<a href="#" title="#"><img class="img-thumbnail" src="public/frontend/images/images/logo.png" title="#" alt="#"/></a>
-<a href="#" title="Tài khoản" class="register"><img src="public/frontend/images/icon-register.png"></a>
+<a href="https://www.5giay.vn" title="5 giây cho mỗi lần thấy"><img class="img-thumbnail" src="public/frontend/images/images/logo.png" title="5 giây cho mỗi lần thấy" alt="5 giây cho mỗi lần thấy"/></a>
+<a href="https://www.5giay.vn/login/login" title="Tài khoản" class="register"><img src="public/frontend/images/icon-register.png"></a>
 </span>
 </div>
 </div>
 <!--/end header fix mobile/-->
 <div id="menu">
 <ul> 
-<li><a href="#" title="Trang chủ">Trang chủ</a></li>
-<li><a href="#" title="Giới thiệu">Giới thiệu</a></li>
-<li><a href="#" title="#">Sản phẩm</a>
+<li><a href="https://www.5giay.vn/" title="Diễn đàn">Diễn đàn</a></li>
+<li><a href="https://www.5giay.vn/forums/gioi-thieu-ve-5giay-vn.68/" title="Giới thiệu">Giới thiệu</a></li>
+<li><a href="" title="Danh mục ngành hàng">Danh mục ngành hàng</a>
 <ul>
-<!--
-<li><a href="#" title="#"><img src="public/frontend/images/images/icon-thoitrang.png" alt="#" class="icon"> Thời trang</a>
-    <ul>
-      <li><a href="#" title="#">aaThời trang nam</a>
-        <ul>
-          <li><a href="#" title="#">Quần thể thao</a></li>
-          <li><a href="#" title="#">Quần thể thao</a></li>
-          <li><a href="#" title="#">Quần thể thao</a></li>
-          <li><a href="#" title="#">Quần thể thao</a></li>
-          <li><a href="#" title="#">Quần thể thao</a></li>
-        </ul>
-      </li>
-      <li><a href="#" title="#">Thời trang nữ</a>
-        <ul>
-          <li><a href="#" title="#">Quần thể thao</a></li>
-          <li><a href="#" title="#">Quần thể thao</a></li>
-          <li><a href="#" title="#">Quần thể thao</a></li>
-          <li><a href="#" title="#">Quần thể thao</a></li>
-          <li><a href="#" title="#">Quần thể thao</a></li>
-        </ul>
-      </li>
-      <li><a href="#" title="#">Phụ kiện</a>
-        <ul>
-          <li><a href="#" title="#">Quần thể thao</a></li>
-          <li><a href="#" title="#">Quần thể thao</a></li>
-          <li><a href="#" title="#">Quần thể thao</a></li>
-          <li><a href="#" title="#">Quần thể thao</a></li>
-          <li><a href="#" title="#">Quần thể thao</a></li>
-        </ul>
-      </li>
-    </ul>
-  </li>
--->
   <?php if(!empty($menu)){ foreach($menu as $k => $val){ ?>
   <li>
       <a href="<?=$url_5giay.'/categories/'.$val['alias'].'.'.$val['id'];?>" title="#"><img src="public/frontend/images/images/icon-dt.png" alt="#" class="icon"><?=$val['name']?></a>
@@ -93,11 +60,14 @@
   <?php } }?> 
 </ul>
 </li>
-<li><a href="#" title="Diễn đàn">Diễn đàn</a></li>
-<li><a href="#" title="#">Sàn đấu giá</a></li>
-<li><a href="#" title="#">Khu vực giữ trật tự cho diễn đàn</a></li>
-<li><a href="#" title="#">Dành cho người mua</a></li>
-<li><a href="#" title="#">Dành cho người bán</a></li>
+<li><a href="https://sohot.vn/auctions/" title="SOHOT" target="_blank">SOHOT</a></li>
+<li><a href="https://www.5giay.vn/forums/cong-nghe.191/" title="PR NEWS">PR NEWS</a></li>
+<li><a href="https://www.haravan.com/harapage?ref=dangkypage" title="HARAPAGE" target="_blank">HARAPAGE</a></li>
+<li><a href="https://www.haravan.com/?ref=dangkypage" title="LÀM WEB RẺ" target="_blank">LÀM WEB RẺ</a></li>
+<li><a href="https://www.5giay.vn/threads/dieu-khoan-ve-bao-ve-thong-tin-ca-nhan.5775002/" title="Điều khoản">Điều khoản</a></li>
+<li><a href="https://www.5giay.vn/igo/trangchu/Qui-che-quan-ly-hoat-dong-5giay.pdf" title="Quy chế">Quy chế</a></li>
+<li><a href="https://new.5giay.vn/threads/noi-quy-cua-5giay-vn.357/" title="Nội quy">Nội quy</a></li>
+<li><a href="https://new.5giay.vn/help/" title="Nội quy">Trợ giúp</a></li>
 </ul>
 </div>
 </header>
